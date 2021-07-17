5c8bfc7c2132a418343acc0b176a403077e96f47
Prajyoth-Reddy-Mothi <72191306+Prajyoth-Reddy-Mothi@users.noreply.github.com>
2021-07-17 23:52:41 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,253 +1,236 @@
 // constants
-const WEB_WORKER_URL = 'scripts/worker.js';
-const BLURBS = {
-  'start': {
-    header: 'Get Ready',
-    blurb: 'Select your difficulty and start the game.'
-  },
-  'p1-turn': {
-    header: 'Your Turn',
-    blurb: 'Click on the board to drop your chip.'
-  },
-  'p2-turn': {
-    header: 'Computer\'s Turn',
-    blurb: 'The computer is trying to find the best way to make you lose.'
-  },
-  'p1-win': {
-    header: 'You Win',
-    blurb: 'You are a winner. Remember this moment. Carry it with you, forever.'
-  },
-  'p2-win': {
-    header: 'Computer Wins',
-    blurb: 'Try again when you\'re done wiping your tears of shame.'
-  },
-  'tie': {
-    header: 'Tie',
-    blurb: 'Everyone\'s a winner! Or loser. Depends on how you look at it.'
-  }
-};
-const OUTLOOKS = {
-  'win-imminent': 'Uh oh, computer is feeling saucy!',
-  'loss-imminent': 'Computer is unsure. Now\'s your chance!'
-};
+const TOTAL_COLUMNS = 7;
+const TOTAL_ROWS = 7;
+const HUMAN_WIN_SCORE = -4;
+const COMPUTER_WIN_SCORE = 4;
+const NO_WIN_SCORE = 0;
 
 // global variables
-var worker;
 var currentGameState;
 
-// document ready
-$(function() {
-  $('.start button').on('click', startGame);
-  setBlurb('start');
-  setOutlook();
+// game state object
+var GameState = function(cloneGameState) {
+  this.board = [];
+  this.score = NO_WIN_SCORE;
+  this.winningChips = undefined;
   
-  // create worker
-  worker = new Worker(WEB_WORKER_URL);
-  worker.addEventListener('message', function(e) {
-    switch(e.data.messageType) {
-      case 'reset-done':
-        startHumanTurn();
-        break;
-      case 'human-move-done':
-        endHumanTurn(e.data.coords, e.data.isWin, e.data.winningChips, e.data.isBoardFull);
-        break;
-      case 'progress':
-        updateComputerTurn(e.data.col);
-        break;
-      case 'computer-move-done':
-        endComputerTurn(e.data.coords, e.data.isWin, e.data.winningChips, e.data.isBoardFull,
-          e.data.isWinImminent, e.data.isLossImminent);
-        break;
+  // initialize an empty board
+  for(var col = 0; col < TOTAL_COLUMNS; col++) {
+    this.board[col] = [];
+  }
+  
+  // clone from existing game state (if one was passed in)
+  if(cloneGameState) {
+    for (var col = 0; col < TOTAL_COLUMNS; col++) {
+      for (var row = 0; row < cloneGameState.board[col].length; row++) {
+        this.board[col][row] = cloneGameState.board[col][row];
+      }
     }
-  }, false);
-});
-
-function setBlurb(key) {
-  $('.info h2').text(BLURBS[key].header);
-  $('.info .blurb').text(BLURBS[key].blurb);
-}
-
-function setOutlook(key) {
-  var $outlook = $('.info .outlook');
-  if(key) {
-    $outlook
-      .text(OUTLOOKS[key])
-      .show();
-  } else {
-    $outlook.hide();
+    this.score = cloneGameState.score;
   }
 }
+GameState.prototype.makeMove = function(player, col) {
+  var coords = undefined;
+  var row = this.board[col].length;
+  if (row < TOTAL_ROWS) {
+    this.board[col][row] = player;
+    this.setScore(player, col, row);
+    coords = { col: col, row: row };
+  }
+  return coords;
+}
+GameState.prototype.isBoardFull = function() {
+  for (var col = 0; col < TOTAL_COLUMNS; col++) {
+    if (this.board[col].length < TOTAL_ROWS) {
+      // found an unfilled column
+      return false;
+    }
+  }
+  return true;
+}
+GameState.prototype.setScore = function(player, col, row) {
+  var isWin =
+      this.checkRuns(player, col, row, 0, 1) || // vertical
+      this.checkRuns(player, col, row, 1, 0) || // horizontal
+      this.checkRuns(player, col, row, 1, 1) || // diagonal "/"
+      this.checkRuns(player, col, row, 1, -1)   // diagonal "\"
+  
+  if(isWin) {
+    this.score = (player === 1) ? HUMAN_WIN_SCORE : COMPUTER_WIN_SCORE;
+  } else {
+    this.score = NO_WIN_SCORE;
+  }
+}
+GameState.prototype.checkRuns = function(player, col, row, colStep, rowStep) {
+  var runCount = 0;
+  
+  // check from 3 chips before to 3 chips after the specified chip
+  // this covers all possible runs of 4 chips that include the specified chip
+  for (var step = -3; step <= 3; step++) {
+    if (this.getPlayerForChipAt(col + step * colStep, row + step * rowStep) === player) {
+      runCount++;
+      if (runCount === 4) {
+        // winning run, step backwards to find the chips that make up this run
+        this.winningChips = [];
+        for(var backstep = step; backstep >= step - 3; backstep--) {
+          this.winningChips.push({
+            col: col + backstep * colStep,
+            row: row + backstep * rowStep
+          });
+        }
+        return true;
+      }
+    } else {
+      runCount = 0;
+      if(step === 0) {
+        // no room left for a win
+        break;
+      }
+    }
+  }
+  
+  // no winning run found
+  return false;
+}
+GameState.prototype.getPlayerForChipAt = function(col, row) {
+  var player = undefined;
+  if (this.board[col] !== undefined && this.board[col][row] !== undefined) {
+    player = this.board[col][row];
+  }
+  return player;
+}
+GameState.prototype.isWin = function() {
+  return (this.score === HUMAN_WIN_SCORE || this.score === COMPUTER_WIN_SCORE);
+}
 
-function startGame() {
-  $('.dif').addClass('freeze');
-  $('.dif input').prop('disabled', true);
-  $('.lit-cells, .chips').empty();
+// listen for messages from the main thread
+self.addEventListener('message', function(e) {
+  switch(e.data.messageType) {
+    case 'reset':
+      resetGame();
+      break;
+    case 'human-move':
+      makeHumanMove(e.data.col);
+      break;
+    case 'computer-move':
+      makeComputerMove(e.data.maxDepth);
+      break;
+  }
+}, false);
 
-  worker.postMessage({
-    messageType: 'reset',
+function resetGame() {
+  currentGameState = new GameState();
+  
+  self.postMessage({
+    messageType: 'reset-done'
   });
 }
 
-function startHumanTurn() {
-  setBlurb('p1-turn');
-  $('.click-columns div').addClass('hover');
-  
-  // if mouse is already over a column, show cursor chip there
-  var col = $('.click-columns div:hover').index();
-  if(col !== -1) {
-    createCursorChip(1, col);
+function makeHumanMove(col) {
+  // coords is undefined if the move is invalid (column is full)
+  var coords = currentGameState.makeMove(1, col);
+  var isWin = currentGameState.isWin();
+  var winningChips = currentGameState.winningChips;
+  var isBoardFull = currentGameState.isBoardFull();
+  self.postMessage({
+    messageType: 'human-move-done',
+    coords: coords,
+    isWin: isWin,
+    winningChips: winningChips,
+    isBoardFull: isBoardFull
+  });
+}
+
+function makeComputerMove(maxDepth) {
+  var col;
+  var isWinImminent = false;
+  var isLossImminent = false;
+  for (var depth = 0; depth <= maxDepth; depth++) {
+    var origin = new GameState(currentGameState);
+    var isTopLevel = (depth === maxDepth);
+    
+    // fun recursive AI stuff kicks off here
+    var tentativeCol = think(origin, 2, depth, isTopLevel);
+    if (origin.score === HUMAN_WIN_SCORE) {
+      // AI realizes it can lose, thinks all moves suck now, keep move picked at previous depth
+      // this solves the "apathy" problem
+      isLossImminent = true;
+      break;
+    } else if (origin.score === COMPUTER_WIN_SCORE) {
+      // AI knows how to win, no need to think deeper, use this move
+      // this solves the "cocky" problem
+      col = tentativeCol;
+      isWinImminent = true;
+      break;
+    } else {
+      // go with this move, for now at least
+      col = tentativeCol;
+    }
   }
   
-  $('.click-columns')
-    .on('mouseenter', function() {
-      var col = $('.click-columns div:hover').index();
-      createCursorChip(1, col);
-    })
-    .on('mouseleave', function() {
-      destroyCursorChip();
-    });
-  
-  $('.click-columns div')
-    .on('mouseenter', function() {
-      var col = $(this).index();
-      moveCursorChip(col);
-    })
-    .on('click', function() {
-      $('.click-columns, .click-columns div').off();
-      
-      var col = $(this).index();
-      worker.postMessage({
-        messageType: 'human-move',
+  var coords = currentGameState.makeMove(2, col);
+  var isWin = currentGameState.isWin();
+  var winningChips = currentGameState.winningChips;
+  var isBoardFull = currentGameState.isBoardFull();
+  self.postMessage({
+    messageType: 'computer-move-done',
+    coords: coords,
+    isWin: isWin,
+    winningChips: winningChips,
+    isBoardFull: isBoardFull,
+    isWinImminent: isWinImminent,
+    isLossImminent: isLossImminent
+  });
+}
+
+function think(node, player, recursionsRemaining, isTopLevel) {
+  var scoreSet = false;
+  var childNodes = [];
+
+  // consider each column as a potential move
+  for (var col = 0; col < TOTAL_COLUMNS; col++) {
+    if(isTopLevel) {
+      self.postMessage({
+        messageType: 'progress',
         col: col
       });
-    });  
-}
-
-function endHumanTurn(coords, isWin, winningChips, isBoardFull) {
-  $('.click-columns div').removeClass('hover');
-  if(!coords) {
-    // column was full, human goes again
-    startHumanTurn();    
-  } else {
-    dropCursorChip(coords.row, function() {
-      if(isWin) {
-        endGame('p1-win', winningChips);
-      } else if(isBoardFull) {
-        endGame('tie');
-      } else {
-        // pass turn to computer
-        startComputerTurn();
+    }
+    
+    // make sure column isn't already full
+    var row = node.board[col].length;
+    if (row < TOTAL_ROWS) {
+      // create new child node to represent this potential move
+      var childNode = new GameState(node);
+      childNode.makeMove(player, col);
+      childNodes[col] = childNode;
+      
+      if(!childNode.isWin() && recursionsRemaining > 0) {
+        // no game stopping win and there are still recursions to make, think deeper
+        var nextPlayer = (player === 1) ? 2 : 1;
+        think(childNode, nextPlayer, recursionsRemaining - 1);
       }
-    });
-  }
-}
 
-function startComputerTurn() {
-  setBlurb('p2-turn');
-  
-  // computer's cursor chip starts far left and moves right as it thinks
-  createCursorChip(2, 0);
-  
-  var maxDepth = parseInt($('input[name=dif-options]:checked').val(), 10) + 1;
-  worker.postMessage({
-    messageType: 'computer-move',
-    maxDepth: maxDepth
-  });
-}
-
-function updateComputerTurn(col) {
-  moveCursorChip(col);
-}
-
-function endComputerTurn(coords, isWin, winningChips, isBoardFull, isWinImminent, isLossImminent) {
-  moveCursorChip(coords.col, function() {
-    dropCursorChip(coords.row, function() {
-      if (isWin) {
-        endGame('p2-win', winningChips);
-      } else if (isBoardFull) {
-        endGame('tie');
-      } else {
-        if(isWinImminent) {
-          setOutlook('win-imminent');
-        } else if (isLossImminent) {
-          setOutlook('loss-imminent');
-        } else {
-          setOutlook();
-        }
-        
-        // pass turn to human
-        startHumanTurn();
+      if (!scoreSet) {
+        // no best score yet, just go with this one for now
+        node.score = childNode.score;
+        scoreSet = true;
+      } else if (player === 1 && childNode.score < node.score) {
+        // assume human will always pick the lowest scoring move (least favorable to computer)
+        node.score = childNode.score;
+      } else if (player === 2 && childNode.score > node.score) {
+        // computer should always pick the highest scoring move (most favorable to computer)
+        node.score = childNode.score;
       }
-    });
-  });
-}
-
-function endGame(blurbKey, winningChips) {
-  $('.dif').removeClass('freeze');
-  $('.dif input').prop('disabled', false);
-  setBlurb(blurbKey);
-  setOutlook();
-  
-  if(winningChips) {
-    // not a tie, highlight the chips in the winning run
-    for(var i = 0; i < winningChips.length; i++) {
-      createLitCell(winningChips[i].col, winningChips[i].row);
     }
   }
-}
 
-function createLitCell(col, row) {
-  $('<div>')
-  .css({
-    'left': indexToPixels(col),
-    'bottom': indexToPixels(row)
-  })
-  .appendTo('.lit-cells');
-}
-
-function createCursorChip(player, col) {
-  var playerClass = 'p' + player;
-  $('<div>', { 'class': 'cursor ' + playerClass })
-    .css('left', indexToPixels(col))
-    .appendTo('.chips');
-  
-  // also highlight column
-  $('.lit-columns div').eq(col).addClass('lit');
-}
-
-function destroyCursorChip() {
-  $('.chips .cursor').remove();
-  $('.lit-columns .lit').removeClass('lit');
-}
-
-function moveCursorChip(col, callback) {
-  $('.chips .cursor').css('left', indexToPixels(col));
-  $('.lit-columns .lit').removeClass('lit');
-  $('.lit-columns div').eq(col).addClass('lit');
-  
-  // callback is only used when the computer is about to drop a chip
-  // give it a slight delay for visual interest
-  setTimeout(callback, 300);
-}
-
-function dropCursorChip(row, callback) {
-  // speed of animation depends on how far the chip has to drop
-  var ms = (7 - row) * 40;
-  var duration = (ms / 1000) + 's';
-  
-  $('.chips .cursor')
-    .removeClass('cursor')
-    .css({
-      'bottom': indexToPixels(row),
-      'transition-duration': duration,
-      'animation-delay': duration
-    })
-    .addClass('dropped');
-  
-  $('.lit-columns .lit').removeClass('lit');
-  setTimeout(callback, ms);
-}
-
-function indexToPixels(index) {
-  return (index * 61 + 1) + 'px';
+  // collect all moves tied for best move and randomly pick one
+  var candidates = [];
+  for (var col = 0; col < TOTAL_COLUMNS; col++) {
+    if (childNodes[col] != undefined && childNodes[col].score === node.score) {
+      candidates.push(col);
+    }
+  }
+  var moveCol = candidates[Math.floor(Math.random() * candidates.length)];
+  return moveCol;
 }
